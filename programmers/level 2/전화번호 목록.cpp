4d2abba0b226580b5eb720242febdca33ae54d18
parkjiyeion <oy5325@gmail.com>
2022-11-12 14:03:45 +0900

    전화번호 목록

---
+++ programmers/level 2/전화번호 목록.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+map <string, int> numbers;
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    int size = phone_book.size();
+    for (int i = 0; i < size; i++) {
+        numbers[phone_book[i]] = 1;
+    }
+    auto before = numbers.begin();
+    auto iter = numbers.begin();
+    for (iter++; iter != numbers.end(); iter++) {
+        string tmp = iter->first;
+        if (tmp.find(before->first) == 0) {
+            return false;
+        }
+
+        before = iter;
+
+    }
+    return answer;
+}
