06ab6049636aa0a0eff91a5393775af4716ad1c6
parkjiyeion <oy5325@gmail.com>
2022-11-13 18:17:33 +0900

    최대 상금

---
+++ SWEA/D3/1244(map).cpp
@@ -0,0 +1,65 @@
+﻿#include<iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+using namespace std;
+int cnt; //교환 횟수
+map<string, vector<int>> board;
+int result = 0; //최종 금액
+string number;
+
+void findmax(int st, int en,int tmpcnt) {
+	//cout << cnt << ": " << tmpcnt << "\n";
+	if (tmpcnt == cnt) { //교환 횟수가 동일할 때
+		//cout <<"current value : " << number << "\n";
+		result = max(result, stoi(number));
+		return;
+	}
+	for (int i = st; i <= en; i++) {
+		for (int j = i + 1; j <= en; j++) {
+			//cout << i << ": " << j << "\n";
+			swap(number[i], number[j]);
+			auto iter = board.find(number);
+			if (iter== board.end()) { //없음
+				//cout << "dont have number" << "\n";
+				//cout << number << ", " << tmpcnt << "\n";
+				board[number].push_back(tmpcnt+1);
+				findmax(i, en,tmpcnt + 1);
+				//swap(number[i], number[j]);
+			}
+			else {
+				vector <int> tmp = iter->second;
+				if (find(tmp.begin(),tmp.end(),tmpcnt+1) == tmp.end()) {
+					////cout << "dont have cnt" << "\n";
+					board[number].push_back(tmpcnt+1);
+					//swap(number[i], number[j]);
+					findmax(i, en, tmpcnt + 1);
+					
+				}
+			}
+			swap(number[i], number[j]);
+		}
+	}
+	
+}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> number >> cnt;
+		//초기화
+		board.clear();
+		result = 0;
+
+		findmax(0, number.length() - 1, 0);
+		cout << "#" << test_case << " " << result << "\n";
+	}
+	return 0;
+}
+
---
+++ SWEA/D3/1244.cpp
@@ -0,0 +1,64 @@
+﻿#include<iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int cnt; //교환 횟수
+vector<int> board;
+int result = 0; //최종 금액
+void settingboard(int number) {
+    int tmp = 0;
+    while (number > 0) {
+        tmp = number % 10;
+        board.push_back(tmp);
+        number = number / 10;
+    }
+    //거꾸로 들어가있음
+    /*for (int i = 0; i < board.size(); i++)
+        cout << board[i] << " ";
+    cout << "\n";*/
+}
+
+void findmax(int left, int en, int tmpcnt) {
+    //cout << cnt << ": " << tmpcnt << "\n";
+    if (tmpcnt == cnt) { //교환 횟수가 동일할 때
+        int sum = 0, num = 1;
+        for (int i = 0; i < board.size(); i++) {
+            sum += board[i] * num;
+            num *= 10;
+        }
+        //cout <<"current value : " << sum << "\n";
+        result = max(result, sum);
+        return;
+    }
+
+    for (int i = left; i <= en; i++) {
+        for (int j = i + 1; j <= en; j++) {
+            swap(board[i], board[j]);
+            findmax(i, en, tmpcnt + 1);
+            swap(board[i], board[j]); //원상복구    
+        }
+    }
+
+
+}
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    cin >> T;
+
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        int number;
+        cin >> number >> cnt;
+        //초기화
+        board.clear();
+        result = 0;
+        settingboard(number);
+        if (cnt > board.size()) cnt = board.size();
+        findmax(0, board.size() - 1, 0);
+        cout << "#" << test_case << " " << result << "\n";
+    }
+    return 0;
+}
