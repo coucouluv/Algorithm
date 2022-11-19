e3ee131d5c857358f0f8e89a81da29db2c2b5ba3
parkjiyeion <oy5325@gmail.com>
2022-11-19 19:59:37 +0900

    부분 수열의 합

---
+++ SWEA/D3/2817.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int A[25];
+int N, K;
+int result = 0;
+void solve(int index, int sum) {
+	if (sum == K) {
+		result += 1;
+		return;
+	}
+	if (sum > K) return;
+	if (index == N) return;
+	for (int i = index; i < N; i++) {
+		solve(i + 1, sum+A[i]);
+	}
+}
+
+int main() {
+
+	int T;
+	cin >> T;
+	
+	for (int test = 1; test <= T; test++) {
+		
+		cin >> N >> K;
+
+		memset(A, 0, sizeof(A)); //초기화
+		result = 0;
+		for (int i = 0; i < N; i++) {
+			cin >> A[i];
+		}
+		solve(0, 0);
+		cout << "#" << test << " " << result << "\n";
+	}
+	return 0;
+}
