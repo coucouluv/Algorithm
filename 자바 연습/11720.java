1b919582d7e0e48bfa97ccf802aba5215147fc4f
parkjiyeion <oy5325@gmail.com>
2022-11-23 01:35:50 +0900

    숫자의 합

---
+++ 자바 연습/11720.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        String number = in.readLine();
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            sum += number.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+}
